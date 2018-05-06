c1cf529c949c7e3d6a971edeede0ac1168630997
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-05-05 23:03:30 -0500

    :computer: body parser added to the project, cors enabled

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const express = require('express')
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
 const app = express()
 const productsRouter = require('./api/routes/products')
 const ordersRouter = require('./api/routes/orders')
 
 app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allor-Headers', 'Origin, X-Requested-With,Content-Type, Accept, Authorization')
+    if (res.method == 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, PATCH');
+        res.json({})
+    }
+})
 
 
 app.use('/products', productsRouter)
 app.use('/orders', ordersRouter)
+
 
 app.use((req, res, next) => {
     const error = new Error('Not found')
