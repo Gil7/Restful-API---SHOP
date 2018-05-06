b0cec2c2ab0c70ee6dd5a68c0988392f9353c460
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-05-06 00:47:21 -0500

    :computer: crud operations to products model ended

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,31 +1,79 @@
 const express = require('express')
-
 const router = express.Router()
 
+
+const Product = require('../models/product')
+
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: "products"
+    Product.find().exec()
+    .then(docs => {
+        console.log(docs)
+        res.status(200).json(docs)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
     })
 })
 router.post('/', (req, res, next) => {
+    const product = new Product({
+        name: req.body.name,
+        price: req.body.price
+    })
+    product.save()
+    .then(result => {console.log(result)})
+    .catch(error => console.log(error))
     res.status(201).json({
-        message: "product created"
+        message: "product created",
+        productCreated : product
     })
 })
 router.get('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: "product details"
+    const productId = req.params.productId
+    Product.findById(productId).exec()
+    .then(doc => {
+        console.log("doc : ", doc)
+        res.status(200).json(doc)
     })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
+    
 })
 router.put('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: "producted updated"
+    const productId = req.params.productId
+    const updateOps = {}
+    for (const key of Object.keys(req.body)) {
+        updateOps[key] = req.body[key]
+    }
+
+    Product.update({_id : productId}, { $set:  updateOps  })
+    .exec()
+    .then( doc => {
+        res.status(200).json(doc)
     })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            error: err
+        })
+    })
+    
 })
 
 router.delete('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: "product deleted"
+    const productId = req.params.productId
+    Product.remove({_id : productId}).exec()
+    .then(result => {
+        console.log(result)
+        res.status(200).json(result)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            error : err
+        })
     })
 })
 module.exports = router
--- app.js
+++ app.js
@@ -1,23 +1,37 @@
 const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
 const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
 const productsRouter = require('./api/routes/products')
 const ordersRouter = require('./api/routes/orders')
 
+mongoose.connect('mongodb://localhost/api-shop')
+
+
+
 app.use(morgan('dev'))
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allor-Headers', 'Origin, X-Requested-With,Content-Type, Accept, Authorization')
-    if (res.method == 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, PATCH');
-        res.json({})
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
     }
-})
-
+    next();
+});
+ 
 
 app.use('/products', productsRouter)
 app.use('/orders', ordersRouter)
